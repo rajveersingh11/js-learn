792b8ab70fe6d66c1c451f5cc476a21617a5acba
Rajveer Singh Shekhawat <119071224+triganometry21@users.noreply.github.com>
2024-07-22 12:08:50 +0000

    Operation of database

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -23,4 +23,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************************** Operations ********************************************
+
+let value = 3
+let negvalue = -value
+ // console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-3);
+//console.log(2*4);
+//console.log(2**3);
+//console.log(2/5);
+//console.log(5%6);
+
+let str1 = "Hello"
+let str2 = "Rajveer"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
