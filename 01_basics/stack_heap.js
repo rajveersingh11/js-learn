a1220e3f1c73a806b7a8467fbb9bc52c32c4b56e
Rajveer Singh Shekhawat <119071224+triganometry21@users.noreply.github.com>
2024-07-24 14:59:26 +0000

    stack_heap

---
+++ 01_basics/stack_heap.js
@@ -0,0 +1,21 @@
+let myYoutubename = "wellbeingThoughts"
+
+let anothername = myYoutubename
+anothername = "unwantederror"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@outlook.com",
+    upi: "user@icici"
+}
+
+let userTwo = userOne
+
+userTwo.email = "rajveer@outlook.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Stack (primitive),  Heap (Non-primitive)
