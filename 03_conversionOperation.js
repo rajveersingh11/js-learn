c80727c9629236159eee62d4cb69f271f565a4d6
Rajveer Singh Shekhawat <119071224+triganometry21@users.noreply.github.com>
2024-07-24 05:20:55 +0000

    comparision of datatypes

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -42,10 +42,16 @@ let str1 = "Hello"
 let str2 = "Rajveer"
 
 let str3 = str1 + str2
-console.log(str3);
+// console.log(str3);
 
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log(true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 
---
+++ 04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 3);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+ // ===
+console.log("2" === 2);
