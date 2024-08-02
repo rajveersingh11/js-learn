52de337dcf5e113751f106d458d35bc75a44775f
Rajveer Singh Shekhawat <119071224+triganometry21@users.noreply.github.com>
2024-08-02 14:16:53 +0000

    date and time

--- 01_basics/07_dateinJs.js
+++ 01_basics/07_dateinJs.js
@@ -9,7 +9,7 @@ let myDate = new Date()
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date("01-24-2023")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
@@ -23,7 +23,7 @@ console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+`${newDate.getDay()} and the time `
 
 newDate.toLocaleString('default', {
     weekday: "long",
